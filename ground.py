52f3c22e07c3b67ca2b212a3ba5f207509611622
cheesyToast67 <elias.burkett@ascte.org>
2026-01-12 07:54:11 -0600

    Create ground.py

---
+++ ground.py
@@ -0,0 +1,12 @@
+class ground:
+    def __init__(self, row, col, color, wid, mining_status, occupy, type, attack_status, damage, health):
+        self.row = row
+        self.col = col
+        self.color = color
+        self.wid = wid
+        self.mining_status = mining_status
+        self.type = type
+        self.attack_status = attack_status
+        self.occupy = occupy
+        self.damage = damage
+        self.health = health
